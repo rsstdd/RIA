fe2d79364577a15f6685123b66c2bb64ef89576b
Ross Todd <rosstod@gmail.com>
2019-02-07 11:33:09 -0800

    feat(ch_01): Add example server using rocket; build and run

---
+++ ch_01_intro_rust/ch-1-time-api/src/main.rs
@@ -0,0 +1,40 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+// Extern - brings external crates into local scope
+extern crate serde;
+extern crate chrono;
+extern crate rocket_contrib;
+
+
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate rocket;
+
+use chrono::prelude::*;
+use rocket_contrib::json::Json;
+
+// Asks compiler to create
+// its own code for converting
+// the Timestamp struct to string
+// to be sent as JSON
+#[derive(Serialize)]
+struct Timestamp {
+    time: String,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/time")]
+fn time_now() -> Json<Timestamp>{
+    let now: DateTime<Utc> = Utc::now();
+    let timestamp = Timestamp { time: now.to_rfc3339() };
+    Json(timestamp)
+}
+
+fn main() {
+    rocket::ignite()
+            .mount("/", routes![index, time_now])
+            .launch();
+}
